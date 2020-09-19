65d584816af38a2c24187738ae49a177ad1d7c7f
Muhammad Qasim <qasim9872@gmail.com>
2020-09-19 20:17:32 +0100

    updated dynamic routes

---
+++ src/controllers/todo-item.js
@@ -0,0 +1,59 @@
+const items = [
+  {
+    id: 1,
+    title: 'hello world',
+    status: false,
+  },
+  {
+    id: 2,
+    title: 'dynamic routes',
+    status: true,
+  },
+];
+
+function readAll() {
+  return items;
+}
+
+function createItem(item) {
+  if (readItem(item.id)) {
+    throw new Error(`item with id: ${item.id} already exists`);
+  }
+
+  items.push(item);
+}
+
+function readItem(id) {
+  return items.find((item) => item.id === id);
+}
+
+function updateItem(newItem) {
+  const id = newItem.id;
+  const index = items.findIndex((item) => item.id === id);
+
+  if (index === -1) {
+    throw new Error(`item with id: ${item.id} doesn't exists`);
+  }
+
+  items[index] = newItem;
+
+  return newItem;
+}
+
+function deleteItem(id) {
+  const index = items.findIndex((item) => item.id === id);
+
+  if (index === -1) {
+    throw new Error(`item with id: ${item.id} doesn't exists`);
+  }
+
+  items.splice(index, 1);
+}
+
+module.exports = {
+  readAll,
+  createItem,
+  readItem,
+  updateItem,
+  deleteItem,
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const port = 3000;
 
 const express = require('express');
+const { json } = require('body-parser');
 const app = express();
 
-const routes = require('./routes');
+app.use(json());
+app.get('/healthcheck', function (req, res) {
+  res.sendStatus(200);
+});
 
-app.use(routes());
+// const routes = require('./dynamic-routes');
+// app.use(routes());
 
 app.listen(port, () => console.log(`api is running on ${port}`));
