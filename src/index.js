fa8222433a3bdb529326f3f33f26695d8bdd274b
Muhammad Qasim <qasim9872@gmail.com>
2020-09-19 16:57:34 +0100

    added healthcheck

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ const port = 3000;
 const express = require('express');
 const app = express();
 
+app.get('/healthcheck', (req, res) => res.sendStatus(200));
+
 app.listen(port, () => console.log(`api is running on ${port}`));
